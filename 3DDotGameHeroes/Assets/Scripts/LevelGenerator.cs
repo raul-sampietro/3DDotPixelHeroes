56f0c91a5438a13d034d2dc86de81971efd71bb6
abadadria <abadadria@gmail.com>
2022-12-01 18:51:20 +0100

    Adjusts the size of the cubes to fit with the size of the player

--- 3DDotGameHeroes/Assets/Scripts/LevelGenerator.cs
+++ 3DDotGameHeroes/Assets/Scripts/LevelGenerator.cs
@@ -20,13 +20,16 @@ public class LevelGenerator : MonoBehaviour
                 for (int z = 0; z < level.height; ++z)
                 {
                     Color pixelColor = level.GetPixel(x, z);
+                    // If the color corresponds to a wall make sure that the orientation is correct
+                    // If the color corresponds to a light that has to be attached to a wall, consider its rotation
+
                     if (pixelColor.a != 0) // Pixel not transparent
                     {
                         foreach (ColorToPrefab colorPrefab in colorMappings)
                         {
                             if (colorPrefab.color.Equals(pixelColor))
                             {
-                                Vector3 position = new(x, count, z);
+                                Vector3 position = new(x*16, 8, z*16);
                                 GameObject obj = Instantiate(colorPrefab.prefab, position, Quaternion.identity, transform);
                                 obj.transform.parent = levelObject.transform;
                             }
