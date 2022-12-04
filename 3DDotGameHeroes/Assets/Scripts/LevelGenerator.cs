7ebb51de3a91fad9fd0f7e88f02b38baf1947669
abadadria <abadadria@gmail.com>
2022-12-04 14:55:57 +0100

    Improves the level generator for the wall case and adds the wall model

--- 3DDotGameHeroes/Assets/Scripts/LevelGenerator.cs
+++ 3DDotGameHeroes/Assets/Scripts/LevelGenerator.cs
@@ -30,13 +30,46 @@ public class LevelGenerator : MonoBehaviour
                         {
                             //Debug.Log("PixelColor: " + pixelColor);
                             //Debug.Log("colorPrefab: " + colorPrefab.color);
+                            //Debug.Log("X: " + x);
+                            //Debug.Log("Z: " + z);
 
-                            if (colorPrefab.color.Equals(pixelColor))
+                            // Color matches and we are not in the corners
+                            if (colorPrefab.color.Equals(pixelColor) & 
+                                !(x == 0 & z == 0) & // Bottom-left corner
+                                !(x == 0 & z == level.height - 1) & // Top-left corner
+                                !(x == level.width - 1 & z == 0) & // Bottom-right corner
+                                !(x == level.width - 1 & z == level.height - 1)) // Top-right corner
                             {
+                                // Calcualte relative position
                                 Vector3 offset = new(16 * (count%3) * sizeOfImage.x, 0, 16 * (int)(count/3) * sizeOfImage.y);
-                                Vector3 position = new(x * 16, 8, z * 16);
+                                Vector3 position = new(x * 16, 0, z * 16);
                                 position += offset;
                                 GameObject obj = Instantiate(colorPrefab.prefab, position, Quaternion.identity, transform);
+                                
+                                
+                                // Scale, rotate and move the asset
+                                switch (colorPrefab.prefab.name)
+                                {
+                                    case "wall":
+                                        Vector3 scale = new(10, 10, 10);
+                                        obj.transform.localScale += scale;
+                                        //obj.transform.Translate(0, 0, 0);
+
+                                        // Rotate according to position X in the level
+                                        if (x == 0) obj.transform.Rotate(0.0f, 90.0f, 0.0f, Space.World); // Left side
+                                        else if (x == level.width - 1) obj.transform.Rotate(0.0f, -90.0f, 0.0f, Space.World); // Right side
+
+                                        // Rotate according to position Y in the level
+                                        if (z == 0) obj.transform.Rotate(0.0f, 0.0f, 0.0f, Space.World); // Bottom side (no rotation needed)
+                                        else if (z == level.height - 1) obj.transform.Rotate(0.0f, -180.0f, 0.0f, Space.World); // Top side
+
+                                        break;
+
+                                    default:
+                                        break;
+                                }                           
+
+                                // Set parent object
                                 obj.transform.parent = levelObject.transform;
                             }
                         }
