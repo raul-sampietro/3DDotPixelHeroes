589bf1ce43c377ef993eedb0d4e9b927b8b92946
raul-sampietro <raul.sampietro@estudiantat.upc.edu>
2022-12-08 19:27:22 +0100

    Added Knight model to MainMenu

--- 3DDotGameHeroes/Assets/Scripts/MenuManager.cs
+++ 3DDotGameHeroes/Assets/Scripts/MenuManager.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Play()
     {
-        
+        SceneManager.LoadScene("TestKnightScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Instructions()
     {
-        
+        SceneManager.LoadScene("InstructionsScene");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
     }
 }
