55bc614ad9bdd978b4d06fb1e28bc946af65fe20
abadadria <abadadria@gmail.com>
2022-12-11 12:55:55 +0100

    The anubis enemy only shoots the player when there are no objects between them (clear shoot)

--- 3DDotGameHeroes/Assets/Scripts/EnemyAnubisMove.cs
+++ 3DDotGameHeroes/Assets/Scripts/EnemyAnubisMove.cs
@@ -24,15 +24,18 @@ public class EnemyAnubisMove : MonoBehaviour
         if (knight == null)
             knight = GameObject.Find("Knight");
 
-        // Locate the direction to reach  the player
-        Vector3 direction = knight.transform.position - transform.position;
-        direction = Vector3.Normalize(direction);
+        if (!Physics.Linecast(transform.position, knight.transform.position))
+        {
+            // Locate the direction to reach  the player
+            Vector3 direction = knight.transform.position - transform.position;
+            direction = Vector3.Normalize(direction);
 
-        // Rotate the enemy to face the player
-        Quaternion rotation = Quaternion.FromToRotation(transform.forward, direction);
-        rotation.ToAngleAxis(out float angle, out Vector3 axis);
-        if (angle > maxRotationSpeed * Time.deltaTime) angle = maxRotationSpeed * Time.deltaTime;
-        if (axis.y < 0.0f) angle = -angle;
-        transform.Rotate(new Vector3(0, 1, 0), angle, Space.World);
+            // Rotate the enemy to face the player
+            Quaternion rotation = Quaternion.FromToRotation(transform.forward, direction);
+            rotation.ToAngleAxis(out float angle, out Vector3 axis);
+            if (angle > maxRotationSpeed * Time.deltaTime) angle = maxRotationSpeed * Time.deltaTime;
+            if (axis.y < 0.0f) angle = -angle;
+            transform.Rotate(new Vector3(0, 1, 0), angle, Space.World);
+        }
     }
 }
--- 3DDotGameHeroes/Assets/Scripts/EnemyAnubisShoot.cs
+++ 3DDotGameHeroes/Assets/Scripts/EnemyAnubisShoot.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class EnemyAnubisShoot : MonoBehaviour
 {
     public float shootingFreq = 0.25f;
-    float timeToShoot;
+    public float timeToShoot;
     public GameObject shot;
+
+    private GameObject knight = null;
 
     // Start is called before the first frame update
     void Start()
@@ -17,19 +19,26 @@ public class EnemyAnubisShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeToShoot -= Time.deltaTime;
-        if (timeToShoot < 0.0f)
-        {
-            // Create the shot
-            timeToShoot = 1.0f / shootingFreq;
-            Instantiate(shot, transform.position + transform.forward + new Vector3(0.0f, 20.0f, 0.0f), transform.rotation);
-            
-        }
-        if (timeToShoot < 0.5f)
-        {
-            // Start the shooting animation
-            gameObject.GetComponent<Animator>().Play("shoot");
-        }
+        // Find the player
+        if (knight == null)
+            knight = GameObject.Find("Knight");
 
+        timeToShoot -= Time.deltaTime;
+
+        if (!Physics.Linecast(transform.position, knight.transform.position))
+        {
+            if (timeToShoot < 0.0f)
+            {
+                // Create the shot
+                timeToShoot = 1.0f / shootingFreq;
+                Instantiate(shot, transform.position + transform.forward + new Vector3(0.0f, 20.0f, 0.0f), transform.rotation);
+
+            }
+            if (timeToShoot < 0.5f)
+            {
+                // Start the shooting animation
+                gameObject.GetComponent<Animator>().Play("shoot");
+            }
+        }
     }
 }
