07c33c69e70c076ee91b888f55b2c80777a51392
abadadria <abadadria@gmail.com>
2022-12-17 18:29:45 +0100

    Implementation of the torch is in progress

---
+++ 3DDotGameHeroes/Assets/Scripts/CandleEffect.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleEffect : MonoBehaviour
+{
+    private Light torchLight;
+
+    public float minIntensity;
+    public float maxIntesity;
+
+    public float minRange;
+    public float maxRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        torchLight = gameObject.transform.GetChild(1).gameObject.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if ((int)(Time.deltaTime * 10000) % 11 == 0)
+        {
+            //Debug.Log((int)(Time.deltaTime * 10000));
+            torchLight.intensity = Random.Range(minIntensity, maxIntesity);
+            torchLight.range = Random.Range(minRange, maxRange);
+        }
+
+    }
+}
