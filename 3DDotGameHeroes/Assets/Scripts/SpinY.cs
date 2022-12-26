73a977715c710575d1c5ad1c650c22537df79ac9
raul-sampietro <raul.sampietro@estudiantat.upc.edu>
2022-12-26 11:46:46 +0100

    Added Boomerang model & made it spin around the Y axis

--- 3DDotGameHeroes/Assets/Scripts/SpinY.cs
+++ 3DDotGameHeroes/Assets/Scripts/SpinY.cs
@@ -2,14 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FixedRotationY : MonoBehaviour
+public class SpinY : MonoBehaviour
 {
     public float rotationSpeed = 30.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
